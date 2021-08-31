00e1346319dd18cbc4ebe608fd2ab6a839fce3bb
Maxim <maxpron1325@gmail.com>
2021-08-31 22:34:56 +0300

    Writing code with tests

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,30 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        return Math.PI * Math.pow(radius,2);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+
+        double k = (double) 4/3;
+
+        return Math.abs(k * Math.PI * Math.pow(radius,3));
     }
 
-    public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+    public static boolean isTrianglePossible(double a, double b, double c)
+    {
+        if(a + b > c & b + c > a & a + c > b) return true;
+
+        else return false;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
-    public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+    public static double getTriangleSquare(double a, double b, double c)
+    {
+        if(!isTrianglePossible(a,b,c) | a < 0 | b < 0 | c < 0) return -1;
+
+        else return Math.abs(Math.sqrt(((a+b+c)/2)*(((a+b+c)/2) - a)*(((a+b+c)/2) - b)*(((a+b+c)/2) - c)));
     }
 }
--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class Main
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+        int countContainers = 1,countTrucks = 1;
+
+        for(int i = 0;i < Integer.parseInt(boxes);i++)
+        {
+            if(i % (27 * 12) == 0){
+                System.out.print("\nГрузовик: " + countTrucks); countTrucks +=1; }
+
+            if(i % 27 == 0){
+                System.out.print("\r\n\tКонтейнер: " + countContainers); countContainers +=1; }
+
+            System.out.print("\r\n\t\t" +"Ящик: " + (i + 1));
+        }
+        System.out.println("\r\nНеобходимо:\r\nгрузовиков - " + (countTrucks-1) + " шт.\r\nконтейнеров - " + (countContainers-1) + " шт.");
+
+
+    // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
 
@@ -20,5 +35,4 @@ public class Main {
         контейнеров - 1 шт.
         */
     }
-
 }
