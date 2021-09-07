23eee58975fb868a01a121453acde93fba101c25
Maxim <maxpron1325@gmail.com>
2021-09-07 21:56:45 +0300

    Using of LocalData class

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,13 +1,25 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.DayOfWeek;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.format.TextStyle;
+import java.util.Calendar;
+import java.util.Date;
+import java.time.*;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 5;
+        int month = 9;
+        int year = 2020;
 
-        System.out.println(collectBirthdays(year, month, day));
+        System.out.println(collectBirthdays(year,month,day));
 
     }
 
@@ -16,7 +28,30 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM y");
+
+        LocalDate today = LocalDate.now();
+
+        LocalDate birthday = LocalDate.of(year,month,day);
+
+        DayOfWeek dayOfWeek = birthday.getDayOfWeek();
+
+        if(birthday.equals(LocalDate.now())) return "0" + " - " + birthday.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)) + " - " + dayOfWeek.getDisplayName(TextStyle.SHORT,Locale.US);
+
+        String period = new String();
+
+        for(int i = 0;i <= LocalDate.now().getYear() - year;i++)
+        {
+            birthday = LocalDate.of(year + i,month,day);
+
+            if(birthday.isBefore(today))
+
+            period += i + " - " + birthday.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)) + " - " + dayOfWeek.getDisplayName(TextStyle.SHORT,Locale.US)  + System.lineSeparator();
+
+            if(birthday.equals(today)) return period += i +" - " + birthday.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)) + " - " + dayOfWeek.plus(1).getDisplayName(TextStyle.SHORT,Locale.US);
+
+        }
+        return period;
     }
 }
--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,30 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args)
+  {
+    LocalDate date  = LocalDate.of(1996,1,21);
+    String period = getPeriodFromBirthday(date);
+    System.out.println(period);
   }
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+  private static String getPeriodFromBirthday(LocalDate birthday)
+  {
+    LocalDate currentData = LocalDate.now();
+
+    int periodOfYears = currentData.getYear() - birthday.getYear();
+
+    int periodOfMonth = currentData.getMonthValue() + (12 - birthday.getMonthValue());
+
+    if(periodOfMonth >= 12){ periodOfYears += periodOfMonth / 12; periodOfMonth = periodOfMonth % 12;}
+
+    int periodOfDays = currentData.getDayOfMonth() + (birthday.lengthOfMonth() - birthday.getDayOfMonth());
+
+    if(periodOfDays > 31){ periodOfMonth += periodOfDays / 31; periodOfDays = periodOfDays % 31;}
+
+    return String.valueOf(periodOfYears) + " years, " + String.valueOf(periodOfMonth) + " month," + String.valueOf(periodOfDays) + " days";
   }
 
 }
