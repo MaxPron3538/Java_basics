cf5ce6bbe33a9f66add9a26dd56d147d93459f89
Maxim <maxpron1325@gmail.com>
2021-09-16 14:06:09 +0300

    Parsing a string and calculation the amount of income

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,44 @@
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static int cycleWord(char[] str)
+  {
+    int count = 0;
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    while (count<str.length){
+
+      if (str[count] >= 48 & str[count] <= 58) count++;
+
+      else return 1;
+    }
+    return 0;
   }
 
+  public static String regSymbol(String text)
+  {
+    if(text.contains(" ")) return " ";
+
+    else return text.substring(text.length()-1);
+  }
+
+  public static void main(String[] args) {
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+    //TODO: напишите ваш код, результат вывести в консоль
+
+    int sumSalary = 0;
+    text = text.trim();
+
+    while (text.length() > 0) {
+
+      String subStr = text.substring(0, text.indexOf(regSymbol(text)));
+      text = text.replaceFirst(subStr + regSymbol(text), "");
+
+      if (cycleWord(subStr.toCharArray()) == 0)
+        sumSalary += Integer.parseInt(String.valueOf(subStr));
+    }
+    System.out.println(sumSalary);
+  }
 }
