cf9e31553c0074a0d53185380648a7f528e204a5
Maxim <maxpron1325@gmail.com>
2021-09-18 14:48:18 +0300

    Splitting a string into an array

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -1,6 +1,33 @@
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+  static String exception = "Введенная строка не является ФИО";
+
+  public static int selectRegularSymbol(String text)
+  {
+    if(text.contains(" ")) return text.indexOf(" ");
+
+    else return text.lastIndexOf(text.substring(text.length()));
+  }
+
+  public static boolean isCyrillic(char arr[]) {
+
+    boolean result = false;
+    for(int i = 0;i < arr.length;i++)
+    {
+      if(arr[i] == ' ' | arr[i] == '-') continue;
+
+      if(Character.isDigit(arr[i])) result = false;
+
+      if (Character.UnicodeBlock.of(arr[i]) == Character.UnicodeBlock.CYRILLIC)
+        result = true;
+      else return result;
+    }
+    return result;
+  }
 
   public static void main(String[] args) {
 
@@ -10,8 +37,29 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+     //TODO:напишите ваш код тут, результат вывести в консоль.
+      // При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      if(input.substring(0,selectRegularSymbol(input)).length() == input.length()){ System.out.println(exception); return;}
+
+      ArrayList<String> fullName  = new ArrayList<>();
+
+      if(isCyrillic(input.toCharArray()))
+      {
+        String partOfName = null;
+
+        while (input.length() > 0)
+         {
+           if(input.substring(0,selectRegularSymbol(input)).equals(partOfName)){ System.out.println(exception); return;}
+
+           partOfName = input.substring(0, selectRegularSymbol(input));
+           input = input.replaceFirst(partOfName, "");
+           fullName.add(partOfName);
+           input = input.trim();
+         }
+         System.out.println("Фамилия: " + fullName.get(0) + "\n" + "Имя: " + fullName.get(1) + "\n" + "Отчество: " + fullName.get(2));
+     }
+     else System.out.println(exception);
     }
   }
 
