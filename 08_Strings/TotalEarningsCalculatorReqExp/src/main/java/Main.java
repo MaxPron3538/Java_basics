d1186bf88a5b36d0ae8e04730fc41e9e5ed4e5ef
Maxim <maxpron1325@gmail.com>
2021-09-20 22:11:33 +0300

    Parsing a string and calculation the amount of income with regular expression

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,47 @@
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+    System.out.println(calculateSalarySum(text));
+
+  }
+
+  public static int containsRegex(String text)
+  {
+    String regex = "\\s";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+
+    if (matcher.find()) return text.indexOf(matcher.group());
+
+    else return text.lastIndexOf(text.substring(text.length()-1));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+
+    int SumSalary = 0;
+    String regexDec = "\\d+";
+    Pattern patternDecimal = Pattern.compile(regexDec);
+
+    while (text.length() > 0)
+    {
+      String subStr = null;
+      subStr = text.substring(0,containsRegex(text));
+      Matcher matcherDecimal = patternDecimal.matcher(subStr);
+
+      if(matcherDecimal.find()) SumSalary += Integer.parseInt(subStr);
+
+      containsRegex(text);
+
+      text = text.replaceFirst(subStr + text.substring(subStr.length(),subStr.length()+1),"");
+    }
+    return SumSalary;
   }
 
 }
