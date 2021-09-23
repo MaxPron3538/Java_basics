167f5c92a1d57b21491e183fbd3b7ce91cc30484
Maxim <maxpron1325@gmail.com>
2021-09-23 18:46:08 +0300

    Checking the correctness of the entered full name

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -12,6 +14,23 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      String regex = "[а-яА-Я-]+\\s[а-яА-Я]+\\s[а-яА-Я]+";
+      Pattern pattern = Pattern.compile(regex);
+      Matcher matcher = pattern.matcher(input);
+
+      if(matcher.matches())
+      {
+        input = input.replaceFirst("","Фамилия:").replaceFirst(" "," Имя:");
+        String partOfInput = input.substring(input.lastIndexOf(" "),input.length());
+        input = input.replaceFirst(partOfInput,"");
+        input += partOfInput.replaceFirst(" "," Отчество:");
+        input = input.replaceAll(" ","\n").replaceAll(":",": ");
+
+        System.out.println(input);
+      }
+      else System.out.println("Введенная строка не является ФИО");
+
     }
   }
 
