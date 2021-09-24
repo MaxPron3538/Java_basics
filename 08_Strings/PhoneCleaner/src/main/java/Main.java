6cc74cba4996024f9f90b8ed1e9acda147de6836
Maxim <maxpron1325@gmail.com>
2021-09-24 09:02:11 +0300

    Checking the correctness of the entered number phone

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +14,22 @@ public class Main {
         break;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
+
+      input = input.replaceAll("-","").replaceAll(" ","");
+      input = input.replaceAll("[(]","")
+              .replaceAll("[)]","");
+      String regex = "[+][7]\\d{10}|[8]\\d{10}|\\d{10}";
+      Pattern pattern = Pattern.compile(regex);
+      Matcher matcher = pattern.matcher(input);
+
+      if (matcher.matches())
+      {
+        if(input.contains("+")) input = input.replaceFirst("[+]","");
+        if(input.contains("8")) input = input.replaceFirst("8","7");
+        if(input.length() == 10) input = input.replaceFirst("","7");
+        System.out.println(input);
+
+      }else System.out.println("Неверный формат номера");
     }
   }
 
