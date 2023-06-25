ea6a30b1b3faad568524b320179172ef196f15cc
Максим Пронь <me@console.local>
2023-06-26 00:33:51 +0300

    Improving performance improvement for writing numbers to a file

--- Performance/CarNumberGenerator/src/Concatenation.java
+++ Performance/CarNumberGenerator/src/Concatenation.java
@@ -3,10 +3,10 @@ public class Concatenation {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
+        StringBuilder builder = new StringBuilder();
 
-        String str = "";
         for (int i = 0; i < 20_000; i++) {
-            str += "some text some text some text";
+            builder.append("some text some text some text");
         }
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,29 +1,58 @@
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        List<PrintWriter> listWriter = new ArrayList<>();
+        List<String> listNumbers = new LinkedList<>();
+        listWriter.add(new PrintWriter("res/numbers1.txt"));
+        listWriter.add(new PrintWriter("res/numbers2.txt"));
 
         char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+        int regionCode = 199;
+        String region = padNumber(regionCode, 2);
+
         for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
+            StringBuilder builder = new StringBuilder();
+            String padNum = padNumber(number, 3);
+
             for (char firstLetter : letters) {
                 for (char secondLetter : letters) {
                     for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
+                        builder.append(firstLetter);
+                        builder.append(padNum);
+                        builder.append(secondLetter);
+                        builder.append(thirdLetter);
+                        builder.append(region);
+                        builder.append("\n");
                     }
                 }
             }
+            listNumbers.add(builder.toString());
         }
 
-        writer.flush();
-        writer.close();
+        Runnable thread1 = () -> {
+            for (int i = 0;i < 500;i++){
+                listWriter.get(0).write(listNumbers.get(i));
+            }
+        };
+
+        Runnable thread2 = () -> {
+            for (int i = 500;i < 999;i++){
+                listWriter.get(1).write(listNumbers.get(i));
+            }
+        };
+
+        Thread run1 = new Thread(thread1);
+        Thread run2 = new Thread(thread2);
+        run1.start();
+        run2.start();
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
@@ -35,7 +64,6 @@ public class Loader {
         for (int i = 0; i < padSize; i++) {
             numberStr = '0' + numberStr;
         }
-
         return numberStr;
     }
 }
